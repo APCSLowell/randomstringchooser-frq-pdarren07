0709598be2786af842b5310cac75807882f37fc4
pdarren07 <pdarren624@gmail.com>
2025-04-27 00:41:15 -0700

    Update RandomLetterChooser.java

--- src/main/java/RandomLetterChooser.java
+++ src/main/java/RandomLetterChooser.java
@@ -1,20 +1,20 @@
 public class RandomLetterChooser extends RandomStringChooser
 {
-	
-	public RandomLetterChooser(String str)
-	{ 
-		/* to be implemented in part (b) */
-		
-	}
-	
-	
-	public static String[] getSingleLetters(String str)
-	{ 
-		String[] letterArray = new String[str.length()];
-		for(int i=0; i<str.length(); i++)
-		{
-			letterArray[i] = ""+str.charAt(i);
-		}
-		return letterArray;
-	}
+
+    public RandomLetterChooser(String str)
+    { 
+        /* to be implemented in part (b) */
+        super(getSingleLetters(str));
+    }
+
+
+    public static String[] getSingleLetters(String str)
+    { 
+        String[] letterArray = new String[str.length()];
+        for(int i=0; i<str.length(); i++)
+        {
+            letterArray[i] = ""+str.charAt(i);
+        }
+        return letterArray;
+    }
 }
