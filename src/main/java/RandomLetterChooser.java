26cdf7c9d1895bc4ae605707a91af2dd6fd21780
pdarren07 <pdarren624@gmail.com>
2025-04-27 00:54:23 -0700

    Update RandomLetterChooser.java

--- src/main/java/RandomLetterChooser.java
+++ src/main/java/RandomLetterChooser.java
@@ -13,7 +13,7 @@ public class RandomLetterChooser extends RandomStringChooser
         String[] letterArray = new String[str.length()];
         for(int i = 0; i < str.length(); i++)
         {
-            letterArray[i] = " " + str.charAt(i);
+            letterArray[i] = "" + str.charAt(i);
         }
         return letterArray;
     }
